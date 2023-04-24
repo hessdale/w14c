e88ec88e7edff34e677ee616637290670ebf6677
dale <hessdale96@gmail.com>
2023-04-23 23:14:27 -0600

    finishing touches and commented my code

--- src/components/ScoreBoard.vue
+++ src/components/ScoreBoard.vue
@@ -9,6 +9,7 @@
 import cookies from "vue-cookies";
 export default {
   methods: {
+    //functions that get cookies to update the wins and losses variable
     update_wins() {
       this.wins = cookies.get(`wins`);
     },
@@ -18,19 +19,22 @@ export default {
   },
   data() {
     return {
-      wins: undefined,
-      loss: undefined,
+      wins: cookies.get(`wins`),
+      loss: cookies.get(`losses`),
     };
   },
 
   mounted() {
+    //sets wins to 0 and sets cookie if cookies are undefined
     if (cookies.get(`wins`) == null) {
       cookies.set(`wins`, 0);
       this.wins = 0;
     }
     if (cookies.get(`losses`) == null) {
       cookies.set(`losses`, 0);
+      this.loss = 0;
     }
+    //listening for data from game button
     this.$root.$on(`wins`, this.update_wins);
     this.$root.$on(`losses`, this.update_loss);
   },
